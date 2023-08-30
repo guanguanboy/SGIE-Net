f56478b228f54c7ae59d10b3661ce8a00be7c9be
liguanlin <517445163@qq.com>
2023-08-30 23:36:26 +0800

    update ssg

--- basicsr/models/archs/ssg_arch.py
+++ basicsr/models/archs/ssg_arch.py
@@ -10,6 +10,15 @@ from torch import nn
 
 from typing import Any, Optional, Tuple, Type
 
+import functools
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import basicsr.models.archs.arch_util as arch_util
+import numpy as np
+import cv2
+#from models.archs.transformer.Models import Encoder_patch66
+
 class MLPBlock(nn.Module):
     def __init__(
         self,
@@ -119,6 +128,90 @@ class MaskEncoder(nn.Module):
 
         return dense_embeddings
 
+
+###############################
+class low_light_transformer(nn.Module):
+    def __init__(self, nf=64, nframes=5, groups=8, front_RBs=5, back_RBs=10, center=None,
+                 predeblur=False, HR_in=False, w_TSA=True):
+        super(low_light_transformer, self).__init__()
+        self.nf = nf
+        self.center = nframes // 2 if center is None else center
+        self.is_predeblur = True if predeblur else False
+        self.HR_in = True if HR_in else False
+        self.w_TSA = w_TSA
+        ResidualBlock_noBN_f = functools.partial(arch_util.ResidualBlock_noBN, nf=nf)
+
+        if self.HR_in:
+            self.conv_first_1 = nn.Conv2d(3, nf, 3, 1, 1, bias=True)
+            self.conv_first_2 = nn.Conv2d(nf, nf, 3, 2, 1, bias=True)
+            self.conv_first_3 = nn.Conv2d(nf, nf, 3, 2, 1, bias=True)
+        else:
+            self.conv_first = nn.Conv2d(3, nf, 3, 1, 1, bias=True)
+
+        self.feature_extraction = arch_util.make_layer(ResidualBlock_noBN_f, front_RBs)
+        self.recon_trunk = arch_util.make_layer(ResidualBlock_noBN_f, back_RBs)
+
+        self.upconv1 = nn.Conv2d(nf*2, nf * 4, 3, 1, 1, bias=True)
+        self.upconv2 = nn.Conv2d(nf*2, 64 * 4, 3, 1, 1, bias=True)
+        self.pixel_shuffle = nn.PixelShuffle(2)
+        self.HRconv = nn.Conv2d(64*2, 64, 3, 1, 1, bias=True)
+        self.conv_last = nn.Conv2d(64, 3, 3, 1, 1, bias=True)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+        #self.transformer = Encoder_patch66(d_model=1024, d_inner=2048, n_layers=6)
+        self.recon_trunk_light = arch_util.make_layer(ResidualBlock_noBN_f, 6)
+
+    def forward(self, x, mask=None):
+        x_center = x #torch.Size([1, 3, 400, 608])
+
+        L1_fea_1 = self.lrelu(self.conv_first_1(x_center)) #torch.Size([1, 64, 400, 608])
+        L1_fea_2 = self.lrelu(self.conv_first_2(L1_fea_1)) #torch.Size([1, 64, 200, 304])
+        L1_fea_3 = self.lrelu(self.conv_first_3(L1_fea_2)) ##torch.Size([1, 64, 100, 152])
+
+        fea = self.feature_extraction(L1_fea_3) #torch.Size([1, 64, 100, 152])
+        fea_light = self.recon_trunk_light(fea) #torch.Size([1, 64, 100, 152])
+
+        h_feature = fea.shape[2]
+        w_feature = fea.shape[3]
+        mask = F.interpolate(mask, size=[h_feature, w_feature], mode='nearest') #对mask进行下采样
+
+        xs = np.linspace(-1, 1, fea.size(3) // 4)
+        ys = np.linspace(-1, 1, fea.size(2) // 4)
+        xs = np.meshgrid(xs, ys)
+        xs = np.stack(xs, 2)
+        xs = torch.Tensor(xs).unsqueeze(0).repeat(fea.size(0), 1, 1, 1).cuda()
+        xs = xs.view(fea.size(0), -1, 2) #torch.Size([1, 950, 2])
+
+        height = fea.shape[2]
+        width = fea.shape[3]
+        fea_unfold = F.unfold(fea, kernel_size=4, dilation=1, stride=4, padding=0)
+        fea_unfold = fea_unfold.permute(0, 2, 1) #torch.Size([1, 950, 1024])
+
+        mask_unfold = F.unfold(mask, kernel_size=4, dilation=1, stride=4, padding=0)
+        mask_unfold = mask_unfold.permute(0, 2, 1)
+        mask_unfold = torch.mean(mask_unfold, dim=2).unsqueeze(dim=-2)
+        mask_unfold[mask_unfold <= 0.5] = 0.0 #torch.Size([1, 1, 950])
+
+        #fea_unfold = self.transformer(fea_unfold, xs, src_mask=mask_unfold)
+        fea_unfold = fea_unfold.permute(0, 2, 1)
+        fea_unfold = nn.Fold(output_size=(height, width), kernel_size=(4, 4), stride=4, padding=0, dilation=1)(fea_unfold)
+
+        channel = fea.shape[1]
+        mask = mask.repeat(1, channel, 1, 1)
+        fea = fea_unfold * (1 - mask) + fea_light * mask
+
+        out_noise = self.recon_trunk(fea)
+        out_noise = torch.cat([out_noise, L1_fea_3], dim=1)
+        out_noise = self.lrelu(self.pixel_shuffle(self.upconv1(out_noise)))
+        out_noise = torch.cat([out_noise, L1_fea_2], dim=1)
+        out_noise = self.lrelu(self.pixel_shuffle(self.upconv2(out_noise)))
+        out_noise = torch.cat([out_noise, L1_fea_1], dim=1)
+        out_noise = self.lrelu(self.HRconv(out_noise))
+        out_noise = self.conv_last(out_noise)
+        out_noise = out_noise + x_center
+
+        return out_noise
+
 if __name__ == '__main__':
     prompt_embed_dim = 120
     image_size = 256
@@ -137,3 +230,10 @@ if __name__ == '__main__':
     x = torch.randn((1,1,image_size,image_size))
     x = mask_encoder(x) 
     print(x.shape) #torch.Size([1, 120, 64, 64])
+
+    light_transformer = low_light_transformer(nf=64, nframes=5,groups=8, front_RBs=1, back_RBs=1,center=None, predeblur=True, HR_in=True)
+    x_mask = torch.randn((1, 1, 400, 600))
+    x = torch.randn((1, 3, 400, 600))
+
+    y = light_transformer(x,x_mask)
+    print('light transformer output shape=', y.shape) #torch.Size([1, 3, 400, 600])
