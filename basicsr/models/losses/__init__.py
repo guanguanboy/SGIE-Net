ab17977cc94b4913529a34aa83733754bf12e82f
liguanlin <517445163@qq.com>
2023-10-19 15:01:51 +0800

    add constrastive learning loss

--- basicsr/models/losses/__init__.py
+++ basicsr/models/losses/__init__.py
@@ -1,5 +1,5 @@
-from .losses import (L1Loss, MSELoss, PSNRLoss, CharbonnierLoss)
+from .losses import (L1Loss, MSELoss, PSNRLoss, CharbonnierLoss, SCSCRLoss)
 
 __all__ = [
-    'L1Loss', 'MSELoss', 'PSNRLoss', 'CharbonnierLoss',
+    'L1Loss', 'MSELoss', 'PSNRLoss', 'CharbonnierLoss', 'SCSCRLoss',
 ]
--- basicsr/models/losses/losses.py
+++ basicsr/models/losses/losses.py
@@ -120,3 +120,206 @@ class CharbonnierLoss(nn.Module):
         # loss = torch.sum(torch.sqrt(diff * diff + self.eps))
         loss = torch.mean(torch.sqrt((diff * diff) + (self.eps*self.eps)))
         return loss
+
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+from torchvision.models import vgg16
+import kornia as K
+import numpy as np
+
+from basicsr.models.losses.color_transfer import pre_process, transfer_chrom, transfer_lum
+
+def inv_norm(tensor):
+    mean = torch.tensor([0.485, 0.456, 0.406])
+    std = torch.tensor([0.229, 0.224, 0.225])
+    if tensor.get_device() > -1:
+        mean = mean.to(tensor.get_device())
+        std = std.to(tensor.get_device())
+    if len(tensor.size()) == 3 and tensor.size(0) == 3:
+        out = tensor * std[:,None,None] + mean[:,None,None]
+    elif len(tensor.size()) == 4 and tensor.size(1) == 3:
+        out = tensor * std[None,:,None,None] + mean[None,:,None,None]
+    else:
+        out = None
+        print(f"Error! Can't normalize shape {tensor.size()}")
+
+    return out
+
+class ParamSmoothness(nn.Module):
+    def __init__(self, pred_name='params'):
+        super(self).__init__()
+        self.pred_outputs = (pred_name, )
+        self.gt_outputs = ()
+
+    def forward(self, params):
+        loss = torch.mean(K.filters.sobel(params))
+        return loss
+    
+class SCSCRLoss(nn.Module):
+
+    def __init__(self, loss_weight=1.0, reduction='mean', pred_name='images', gt_image_name='target_images', gt_comp_name='images', k=5, writer=None):
+        super(SCSCRLoss, self).__init__()
+        assert reduction == 'mean'
+        self.loss_weight = loss_weight
+        self.pred_outputs = (pred_name, )
+        self.gt_outputs = (gt_image_name, gt_comp_name)
+
+        self.D = nn.L1Loss()
+        self.k = k
+        self.writer = writer
+        VGG16_model = vgg16(pretrained=True)
+        self.SR_extractor = VGG16_model.features[:23]
+
+    def forward(self, pred, gt_image, input_lowlight, return_neg_samp=False):
+        '''
+        composition (N, 3, H, W):           composition image (input image to the network)
+        pred        (N, 3, H, W):           predicted image (output of the network)
+        gt_image    (N, 3, H, W):           ground truth image
+        mask        (N, 1, H, W):           image mask         
+        '''
+        composition = input_lowlight
+
+        if self.k > pred.size(0)-1:
+            self.k = pred.size(0)-1
+
+        # create negative samples
+        with torch.no_grad():
+            neg_samp = self.create_negative_samples(gt_image.detach(), composition.detach())
+
+        if return_neg_samp:
+            return neg_samp
+
+        # calculate foreground and background style representations    
+        self.SR_extractor = self.SR_extractor.to(pred.get_device())
+        self.SR_extractor.eval()
+
+        f = self.SR_extractor(pred)                                            # (N, 512, 32, 32)
+        f_plus = self.SR_extractor(gt_image)                                   # (N, 512, 32, 32)
+        f_minus = [self.SR_extractor(neg_samp[:,k]) for k in range(self.k)]    # (K, N, 512, 32, 32)
+        #b_plus = self.SR_extractor(gt_image * (1-mask))                               # (N, 512, 32, 32)
+        
+        # self-style contrastistive regularization (SS-CR)
+        l_ss_cr = self.D(f, f_plus) / ( self.D(f, f_plus) + torch.sum(torch.tensor([self.D(f, f_minus_k) for f_minus_k in f_minus])) + 1e-8 )
+
+        return self.loss_weight * l_ss_cr
+
+    def create_negative_samples(self, gt_images, composition, gamma=2.2):
+        '''
+        samples K-1 negative samples for each composited image based on the other images in the batch
+
+        gt_images   (N, 3, H, W):           ground truth images
+        composition (N, 3, H, W):           composition image (input image to the network)
+        mask        (N, 1, H, W):           image mask   
+
+        neg_samp    (N, K, 3, H, W):        K negative samples 
+        '''
+
+        # Pre-processing
+        N, C, H, W, = gt_images.size()        
+        img = inv_norm(gt_images)
+        img = torch.pow(img, gamma)
+        img = K.color.rgb_to_lab(img)
+
+        for n in range(N):
+            img[n, 0] = pre_process(img[n, 0].clone())
+
+        # Change color statistics for each composition
+        neg_samp = torch.ones((N, self.k-1, C, H, W)).to(gt_images.get_device())
+        ref_img = np.zeros((N, self.k-1), np.int16)
+        for n in range(N):
+            for j, n2 in enumerate(np.random.choice([i for i in range(N) if i!=n], self.k-1, replace=False)):
+
+                ## No mask consideration for color transformation                
+                c_img = transfer_chrom(img[n], img[n2])                                                 # (3, H, W)                           
+                t_img = transfer_lum(img[n, 0], img[n2, 0])
+                
+                neg_img = torch.stack([t_img, c_img[1], c_img[2]])                # (3, H, W)
+      
+                neg_samp[n, j] = neg_img
+                ref_img[n, j] = n2
+
+        # Post-processing
+        neg_samp = K.color.lab_to_rgb(neg_samp)
+        neg_samp = torch.pow(neg_samp, 1/gamma)
+        normalize = T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        neg_samp = normalize(neg_samp)
+        neg_samp = torch.cat([composition.unsqueeze(1), neg_samp], dim=1)
+
+        return neg_samp
+
+    def Gram(self, mat1, mat2):
+        '''
+        caculates the Gram matrix
+
+        mat1 (N, 512, 32, 32):              feature map
+        mat2 (N, 512, 32, 32):              feature map
+
+        out (N, 512, 512):                  Gram matrix of both feature maps
+        '''
+
+        out = []
+        for f1, f2 in zip(mat1, mat2):
+            out.append( torch.matmul(f1.view(512, -1).T, f2.view(512, -1)) )
+
+        return torch.stack(out)
+    
+
+# def gradient(input_tensor, direction):
+#     smooth_kernel_x = torch.reshape(torch.tensor([[0, 0], [-1, 1]], dtype=torch.float32), [2, 2, 1, 1])
+#     smooth_kernel_y = torch.transpose(smooth_kernel_x, 0, 1)
+#     if direction == "x":
+#         kernel = smooth_kernel_x
+#     elif direction == "y":
+#         kernel = smooth_kernel_y
+#     gradient_orig = torch.abs(torch.nn.conv2d(input_tensor, kernel, strides=[1, 1, 1, 1], padding='SAME'))
+#     grad_min = torch.min(gradient_orig)
+#     grad_max = torch.max(gradient_orig)
+#     grad_norm = torch.div((gradient_orig - grad_min), (grad_max - grad_min + 0.0001))
+#     return grad_norm
+
+# class SmoothLoss(nn.Moudle):
+#     """ illumination smoothness"""
+
+#     def __init__(self, loss_weight=0.15, reduction='mean', eps=1e-2):
+#         super(SmoothLoss,self).__init__()
+#         self.loss_weight = loss_weight
+#         self.eps = eps
+#         self.reduction = reduction
+    
+#     def forward(self, illu, img):
+#         # illu: b×c×h×w   illumination map
+#         # img:  b×c×h×w   input image
+#         illu_gradient_x = gradient(illu, "x")
+#         img_gradient_x  = gradient(img, "x")
+#         x_loss = torch.abs(torch.div(illu_gradient_x, torch.maximum(img_gradient_x, 0.01)))
+
+#         illu_gradient_y = gradient(illu, "y")
+#         img_gradient_y  = gradient(img, "y")
+#         y_loss = torch.abs(torch.div(illu_gradient_y, torch.maximum(img_gradient_y, 0.01)))
+
+#         loss = torch.mean(x_loss + y_loss) * self.loss_weight
+
+#         return loss
+
+# class MultualLoss(nn.Moudle):
+#     """ Multual Consistency"""
+
+#     def __init__(self, loss_weight=0.20, reduction='mean'):
+#         super(MultualLoss,self).__init__()
+
+#         self.loss_weight = loss_weight
+#         self.reduction = reduction
+    
+
+#     def forward(self, illu):
+#         # illu: b x c x h x w
+#         gradient_x = gradient(illu,"x")
+#         gradient_y = gradient(illu,"y")
+
+#         x_loss = gradient_x * torch.exp(-10*gradient_x)
+#         y_loss = gradient_y * torch.exp(-10*gradient_y)
+
+#         loss = torch.mean(x_loss+y_loss) * self.loss_weight
+#         return loss
